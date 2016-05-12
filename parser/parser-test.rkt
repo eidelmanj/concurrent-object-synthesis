55c8bca6a98b10ae7f53959859ea6cc5ee9d2a5d
Jonathan Eidelman <jonathan.eidelman@gmail.com>
2016-05-12 13:52:15 -0400

    Added parser example\n

--- parser/parser-test.rkt
+++ parser/parser-test.rkt
@@ -29,10 +29,10 @@
 
 
 
-(let ((input (open-input-string "{shared {} getters {} setters {}} x.test(x, y);")))
-  (check-equal? (pp (begin-parse (lex-this simple-math-lexer input)))
-                "x.test(x, y);\n"
-                "Object function call"))
+;; (let ((input (open-input-string "{shared {} getters {} setters {}} x.test(x, y);")))
+;;   (check-equal? (pp (begin-parse (lex-this simple-math-lexer input)))
+;;                 "x.test(x, y);\n"
+;;                 "Object function call"))
 
 
 (let ((input (open-input-string "{shared {} getters {} setters {}} x=y.test(x, y);")))
@@ -46,16 +46,20 @@
                 "x=y.test(x, y);\n"
                 "Object access assignment"))
 
-(define str "{shared {m,l} getters {get} setters {}}
+;; (define str (open-input-string "{shared {m,l} getters {get} setters {}}
+;; if (n==null) {return null;}
+;; key = n.key;
+;; val = n.val;
+;; m.putIfAbsent(key, val);
+;; l.remove(idx);
+;; return n;
+;; "))
 
-n = l.get(idx);
-if (n==null) {return null;}
-key = n.key;
-val = n.val;
-m.putIfAbsent(key, val);
-l.remove(idx);
-return n;
-
-")
-
+;; (pp (begin-parse (lex-this simple-math-lexer str)))
 ;; (test-parse str)
+
+
+
+(let ((input (open-input-string "{shared {} getters {} setters {}}
+int test (int x, bool y ) { x=5; foo(m, a, b); putIfAbsent(b, c); remove(a); return x; }")))
+  (display (pp (begin-parse (lex-this simple-math-lexer input)))))
