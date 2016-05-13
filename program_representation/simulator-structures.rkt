4896c8d535f85506998356e04e58f96a9d7b0a16
Jonathan Eidelman <jonathan.eidelman@gmail.com>
2016-05-13 16:50:00 -0400

    added example

--- program_representation/simulator-structures.rkt
+++ program_representation/simulator-structures.rkt
@@ -14,12 +14,21 @@
 
 ;; Data structure for method
 (define-struct Method (id instr-list))
+;; id - name of method
+;; instr-list - for now just list of Instruction structures
 
 ;; Data structure for representing instructions that should be concurrent
 (define-struct Thread-list (instr-list))
 
 ;; Data structure containing an instruction from a program - also shows whether it is part of the client or the method sketch
 (define-struct Instruction (i is-method id atomic inner-id))
+;; i - (lambda (e) (amap-putIfAbsent(e "m" "key" "val"  "")))
+;; i - this is the actual function ie (lambda (e) (update-thing e "jfeioa" fhfeoia" "ret"))
+;; id - always 0
+;; is-method - always true
+;; atomic - always false
+;; inner-id - line number? Unique number doesn't matter
+
 
 (define-struct Branch (condition branch1 branch2))
 (define-struct Loop (condition instr-list))
