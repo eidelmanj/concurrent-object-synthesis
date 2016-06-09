d34478c3befb4283ffa6c2af77165e1d2043ebc3
Jonathan Eidelman <eidelmanj@mail.cs.toronto.edu>
2016-06-09 09:46:40 -0400

    added new C based structures

--- program_representation/simulator-structures.rkt
+++ program_representation/simulator-structures.rkt
@@ -13,14 +13,67 @@
  Assume?
  Assume-condition
  (struct-out Assume-meta)
+ (struct-out None)
  (struct-out Assume-simulation)
  (struct-out Continue)
  (struct-out Empty)
  (struct-out Meta-information)
+ (struct-out Hole)
+ (struct-out Lock)
+ (struct-out Unlock)
+ (struct-out Create-var)
+ (struct-out Get-var)
+ (struct-out Return)
+ (struct-out Dereference)
+ (struct-out Equal)
+ (struct-out Not)
+ (struct-out Or)
+ (struct-out Set-var)
+ (struct-out Run-method)
+ (struct-out Set-pointer)
+ (struct-out And)
+ (struct-out Get-argument)
+ (struct-out Arguments)
+ (struct-out History)
+ (struct-out Operation)
+ (struct-out Interval)
+ (struct-out Info)
  (struct-out Sketch-placeholder))
 
 
 
+
+
+(define-struct History (start-env op-list))
+(define-struct Operation (op interval id))
+(define-struct Interval (start end))
+
+
+
+
+(define-struct Set-pointer (id type offset val instr-id))
+
+(define-struct Run-method (method args ret ))
+(define-struct Lock (id instr-id))
+
+(define-struct Create-var (id type instr-id))
+(define-struct Set-var (id assignment instr-id))
+(define-struct Unlock (id instr-id))
+(define-struct Get-var (id))
+(define-struct Return (val instr-id))
+(define-struct Dereference (id type offset))
+(define-struct Equal (expr1 expr2))
+(define-struct Not (expr))
+(define-struct Or (expr1 expr2))
+(define-struct And (expr1 expr2))
+(define-struct Get-argument (id))
+(define-struct Arguments (arg-list))
+
+
+
+(struct None ())
+
+(define-struct Hole (method1 interruptor  method2))
 
 ;; Data structure for client
 (define-struct Client-pre (instr-list))
@@ -35,6 +88,12 @@
 
 ;; Data structure containing an instruction from a program - also shows whether it is part of the client or the method sketch
 (define-struct Instruction (i is-method id atomic inner-id rw? meta))
+
+
+
+(struct Info (thread-id instr-id) #:transparent)
+
+
 ;; i - (lambda (e) (amap-putIfAbsent(e "m" "key" "val"  "")))
 ;; i - this is the actual function ie (lambda (e) (update-thing e "jfeioa" fhfeoia" "ret"))
 ;; id - always 0
@@ -53,8 +112,8 @@
 (define-struct Branch (condition branch1 branch2))
 (define-struct Meta-branch (condition branch1 branch2))
 
-(define-struct Single-branch (condition branch))
-(define-struct Loop (condition instr-list))
+(define-struct Single-branch ( condition branch))
+(define-struct Loop ( condition instr-list))
 
 
 
