2def70383f12f1eccfb36fbc73ca8b6637f0ab6c
Aliya Hameer <adhameer@gmail.com>
2016-06-24 18:58:37 -0400

    Write an interpreter for sequential ASTs.

---
+++ error_trace_generation/interpret.rkt
@@ -0,0 +1,358 @@
+#lang racket/base
+
+(require "../program_representation/simulator-structures.rkt"
+         (only-in racket/match match match-lambda define-match-expander)
+         racket/format)
+
+(provide make-interpreter)
+
+(define reserved-parameters-keyword 'parameters-reserved)
+
+; For backwards compatibility with the old format.
+; The q is short for quote. It needs a better name though.
+(define-match-expander q
+  (syntax-rules ()
+    [(q id) (app to-symbol id)]))
+
+; Return a function that takes a list of instructions and executes them
+;  with the bindings associated with the functions in library.
+(define (make-interpreter library)
+  ; Given an argument type, generate a hopefully unique parameter name.
+  (define make-arg
+    (let ([counter -1])
+      (λ (type)
+        (set! counter (add1 counter))
+        (string->symbol (format "~a~a" type counter)))))
+
+  ; These will be evaluated to instantiate the library method definitions
+  ;  in the execution environment.
+  (define library-defs
+    (map
+     (match-lambda
+       [(Method (q id) args ret-type instr-list)
+        (define parameters (map make-arg args))
+        `(define (,id . ,parameters)
+           ; Support for the return keyword
+           (let/cc return
+             ; A list of parameters for Get-argument
+             (define ,reserved-parameters-keyword (list . ,parameters))
+             . ; Method body
+             ,(map transform instr-list)))])
+     library))
+
+  ; This namespace requires the larger racket library and then instantiates
+  ;  the library method definitions so compiled programs can just call the
+  ;  methods without doing any extra work.
+  (define lib-namespace
+    (parameterize ([current-namespace (make-base-empty-namespace)])
+      (namespace-require 'racket)
+      (namespace-require 'racket/block) ; see below for why we need this
+      (map eval library-defs)
+
+      ; Important struct definitions. Node is temporary.
+      (eval '(struct None ()))
+      (eval '(struct Node (next key val bits) #:mutable))
+
+      (current-namespace)))
+
+  ; A closure that evaluates the given AST in the context of the library
+  ;  method definitions and returns a map of the variables of interest to
+  ;  their values.
+  ; All variables of interest MUST be declared in global scope of the AST.
+  (λ (ast vars-of-interest)
+    ; Constructs a statement that when passed to eval, constructs a hash
+    ;  table of the names of the variables of interest to their values.
+    (define var-hash
+      `(make-hash (list . ,(map (λ (var-name)
+                                  `(cons (quote ,var-name) ,var-name))
+                                vars-of-interest))))
+
+    (eval
+     ; Use block instead of begin so variables will be defined in a new
+     ;  scope. This is necessary because otherwise calling eval would
+     ;  transform the namespace.
+     `(block . ,(append
+                 (map transform ast)
+                 `(,var-hash)))
+     lib-namespace)))
+
+; For now, assume name is either a string or a symbol.
+; This is just for backwards compatibility with the old format.
+(define (to-symbol name)
+  (if (string? name)
+      (string->symbol name)
+      name))
+
+; Return an accessor of the form (struct-id-field id).
+(define (to-accessor id struct-id field)
+  ; For now, assume all three arguments are strings.
+  (list (string->symbol (format "~a-~a" struct-id field))
+        (to-symbol id)))
+
+; Return a mutator of the form (set-struct-id-field! id value).
+(define (to-mutator id struct-id field value)
+  ; For now, assume the first three arguments are strings.
+  (list (string->symbol (format "set-~a-~a!" struct-id field))
+        (to-symbol id)
+        value))
+
+; For recursively transforming subexpressions.
+; I had no idea what to call it, so for now it's just x.
+(define-match-expander x
+  (syntax-rules ()
+    [(x expr) (app transform expr)]))
+
+; Transform an AST into a quoted S-expression.
+(define (transform instr)
+  (match instr
+    [(Create-var _ _ (q id) _) `(define ,id (void))]
+    [(Set-var _ _ (q id) (x assignment)) `(set! ,id ,assignment)]
+    [(Get-var (q id)) id]
+    [(Set-pointer _ _ (x id) type offset (x val)) (to-mutator id type offset val)]
+    [(Return _ _ (x expr)) `(return ,expr)]
+    [(New-struct (q type) parameters) `(,type . ,(map transform parameters))]
+
+    ; Method parameters are stored in a list with id reserved-parameters-keyword.
+    [(Get-argument _ _ index) `(list-ref ,reserved-parameters-keyword ,index)]
+
+    ; This compiler is for single-thread runs only, so just ignore locks
+    ;  and don't worry about making CAS atomic.
+    [(Lock _ _ id) '(void)]
+    [(Unlock _ _ id) '(void)]
+    [(CAS _ _ p1 p2 new ret) (transform
+                              (Set-var ret
+                                       (Branch (Equal p1 p2)
+                                               `(,(match p1
+                                                    [(Dereference id type offset)
+                                                     (Set-pointer id type offset new)]
+                                                    [(Get-var id) (Set-var id new)])
+                                                 #t)
+                                               '(#f))))]
+
+    ; Method calls
+    [(Run-method _ _ (q id) args '()) `(,id ,@(map transform args))]
+    [(Run-method _ _ (q id) args (q ret)) `(set! ,ret (,id . ,(map transform args)))]
+
+    ; Conditionals
+    [(Single-branch _ _ (x condition) branch) (append
+                                               `(when ,condition)
+                                               (map transform branch))]
+    [(Branch _ _ (x condition) b1 b2) `(if ,condition
+                                           (begin . ,(map transform b1))
+                                           (begin . ,(map transform b2)))]
+
+    ; Loops
+    [(Loop _ _ (x condition) body) `(let loop ()
+                                      (when ,condition
+                                        ,(append
+                                          '(let/cc continue)
+                                          (map transform body))
+                                        (loop)))]
+    [(Continue _) '(continue)]
+
+    ; Subexpressions
+    [(Equal (x expr1) (x expr2)) `(equal? ,expr1 ,expr2)]
+    [(Not (x expr)) `(not ,expr)]
+    [(And (x expr1) (x expr2)) `(and ,expr1 ,expr2)]
+    [(Or (x expr1) (x expr2))  `(or ,expr1 ,expr2)]
+    [(Is-none? (x expr)) `(None? ,expr)]
+    [(None) '(None)]
+    [(Dereference (x id) type offset) (to-accessor id type offset)]
+    [(Add (x expr1) (x expr2)) `(+ ,expr1 ,expr2)]
+    [(Subtract (x expr1) (x expr2)) `(- ,expr1 ,expr2)]
+    [(Less-than (x expr1) (x expr2)) `(< ,expr1 ,expr2)]
+    [(Less-than-equal (x expr1) (x expr2)) `(<= ,expr1 ,expr2)]
+    [(Greater-than (x expr1) (x expr2)) `(> ,expr1 ,expr2)]
+    [(Greater-than-equal (x expr1) (x expr2)) `(>= ,expr1 ,expr2)]
+
+    ; Literals
+    [_ instr]))
+
+(module+ test
+  (require rackunit)
+
+  ; Basic subexpressions
+  (check-equal? (transform (Equal 1 2)) '(equal? 1 2))
+  (check-equal? (transform (Not #t)) '(not #t))
+  (check-equal? (transform (And #t #f)) '(and #t #f))
+  (check-equal? (transform (Or #t #f)) '(or #t #f))
+  (check-equal? (transform (Is-none? 'x)) '(None? x))
+  (check-equal? (transform (None)) '(None))
+  (check-equal? (transform (Dereference 'test 'Node 'key)) '(Node-key test))
+  (check-equal? (transform (Add 1 2)) '(+ 1 2))
+  (check-equal? (transform (Subtract 1 2)) '(- 1 2))
+  (check-equal? (transform (Less-than 1 2)) '(< 1 2))
+  (check-equal? (transform (Less-than-equal 1 2)) '(<= 1 2))
+  (check-equal? (transform (Greater-than 1 2)) '(> 1 2))
+  (check-equal? (transform (Greater-than-equal 1 2)) '(>= 1 2))
+
+  ; Simple C instructions
+  (check-equal? (transform (Create-var 'x 'int)) '(define x (void)))
+  (check-equal? (transform (Set-var 'x 1)) '(set! x 1))
+  (check-equal? (transform (Get-var 'x)) 'x)
+  (check-equal? (transform (Set-pointer 'test 'Node 'key 1)) '(set-Node-key! test 1))
+  (check-equal? (transform (Lock 0)) '(void))
+  (check-equal? (transform (Unlock 0)) '(void))
+  (check-equal? (transform (Return 'x)) '(return x))
+  (check-equal? (transform (Continue 0)) '(continue))
+  (check-equal? (transform (New-struct 'Node (list (None) 1 2 0))) '(Node (None) 1 2 0))
+  (check-equal? (transform (Get-argument 0)) `(list-ref ,reserved-parameters-keyword 0))
+  (check-equal? (transform (Run-method 'remove (list (None) 1) null))
+                '(remove (None) 1))
+  (check-equal? (transform (Run-method 'get (list (None) 1) 'val))
+                '(set! val (get (None) 1)))
+
+  ; Compound C instructions
+  (check-equal? (transform (Single-branch #t (list (Add 1 2)))) '(when #t (+ 1 2)))
+  (check-equal? (transform (Branch #t (list (Add 1 2)) (list (Add 3 4))))
+                '(if #t (begin (+ 1 2)) (begin (+ 3 4))))
+  (check-equal? (transform (Loop #t (list (Create-var 'x 'int) (Set-var 'x 1))))
+                '(let loop ()
+                   (when #t
+                     (let/cc continue
+                       (define x (void))
+                       (set! x 1))
+                     (loop))))
+  (check-equal? (transform (CAS (Dereference 'x 'Node 'key)
+                                (Dereference 'y 'Node 'key)
+                                (Add 1 2)
+                                'z))
+                '(set! z (if (equal? (Node-key x) (Node-key y))
+                             (begin
+                               (set-Node-key! x (+ 1 2))
+                               #t)
+                             (begin
+                               #f))))
+
+  ; Some test library methods
+  (define push-instrs
+    `(,(Lock 1)
+      ,(Create-var "cur" "Node")
+      ,(Create-var "prev" "Node")
+      ,(Set-var "cur" (Get-argument 0))
+      ,(Set-var "prev" (Get-argument 0))
+      ,(Loop (And (Not (Is-none? (Get-var "cur")))
+                  (Not (Equal (Dereference "cur" "Node" "key") (Get-argument 1))))
+             `(,(Set-var "prev" (Get-var "cur"))
+               ,(Set-var "cur" (Dereference "cur" "Node" "next"))))
+      ,(Single-branch
+        (Is-none? (Get-var "cur"))
+        `(,(Set-pointer "prev" "Node" "next"
+                        (New-struct "Node" `(,(None)
+                                             ,(Get-argument 1)
+                                             ,(Get-argument 2)
+                                             ,(None))))
+          ,(Unlock 1)
+          ,(Return (Get-argument 2))))
+
+      ,(Set-pointer "cur" "Node" "val" (Get-argument 2))
+      ,(Unlock 1)
+      ,(Return (Get-argument 2))))
+  (check-equal? (map transform push-instrs)
+                `((void)
+                  (define cur (void))
+                  (define prev (void))
+                  (set! cur (list-ref ,reserved-parameters-keyword 0))
+                  (set! prev (list-ref ,reserved-parameters-keyword 0))
+                  (let loop ()
+                    (when (and (not (None? cur))
+                               (not (equal? (Node-key cur)
+                                            (list-ref ,reserved-parameters-keyword 1))))
+                      (let/cc continue
+                        (set! prev cur)
+                        (set! cur (Node-next cur)))
+                      (loop)))
+                  (when (None? cur)
+                    (set-Node-next! prev (Node
+                                          (None)
+                                          (list-ref ,reserved-parameters-keyword 1)
+                                          (list-ref ,reserved-parameters-keyword 2)
+                                          (None)))
+                    (void)
+                    (return (list-ref ,reserved-parameters-keyword 2)))
+                  (set-Node-val! cur (list-ref ,reserved-parameters-keyword 2))
+                  (void)
+                  (return (list-ref ,reserved-parameters-keyword 2))))
+
+  (define get-instrs
+    `(,(Lock 1)
+      ,(Create-var "cur" "Node")
+      ,(Set-var "cur" (Get-argument 0))
+      ,(Loop  (And (Not (Is-none? (Get-var "cur")))
+                   (Not (Equal (Dereference "cur" "Node" "key") (Get-argument 1))))
+              `(,(Set-var "cur" (Dereference "cur" "Node" "next"))))
+      ,(Single-branch
+        (Is-none? (Get-var "cur"))
+        `(,(Unlock 1)
+          ,(Return 0)))
+      ,(Unlock 1)
+      ,(Return (Dereference "cur" "Node" "val"))))
+  (check-equal? (map transform get-instrs)
+                `((void)
+                  (define cur (void))
+                  (set! cur (list-ref ,reserved-parameters-keyword 0))
+                  (let loop ()
+                    (when (and (not (None? cur))
+                               (not (equal? (Node-key cur)
+                                            (list-ref ,reserved-parameters-keyword 1))))
+                      (let/cc continue
+                        (set! cur (Node-next cur)))
+                      (loop)))
+                  (when (None? cur)
+                    (void)
+                    (return 0))
+                  (void)
+                  (return (Node-val cur))))
+
+  (define contains-instrs
+    `(,(Create-var "val" "int")
+      ,(Run-method "get" (list (Get-argument 0) (Get-argument 1)) "val")
+      ,(Return (Get-var "val"))))
+  (check-equal? (map transform contains-instrs)
+                `((define val (void))
+                  (set! val (get (list-ref ,reserved-parameters-keyword 0)
+                                 (list-ref ,reserved-parameters-keyword 1)))
+                  (return val)))
+
+  ; Test make-interpreter
+  (define-syntax-rule (check-var-equal? <id> expected instr ...)
+    (check-equal? (hash-ref (interpret `(,instr ...)
+                                       '(<id>))
+                            '<id>)
+                  expected))
+
+  (define test-library
+    `(,(Method 'push '(Node int int) 'int push-instrs)
+      ,(Method 'get '(Node int) 'int get-instrs)
+      ,(Method 'contains '(Node int) 'int contains-instrs)))
+  (define interpret (make-interpreter test-library))
+  ; First node in the list
+  (check-var-equal? ret 3
+                    (Create-var 'ret 'int)
+                    (Run-method 'get
+                                '((Node
+                                   (Node (None) 1 3 0)
+                                   2 4 0)
+                                  1)
+                                'ret)
+                    (Get-var 'ret))
+  ; Last node in the list
+  (check-var-equal? ret 4
+                    (Create-var 'ret 'int)
+                    (Run-method 'get
+                                '((Node
+                                   (Node (None) 1 3 0)
+                                   2 4 0)
+                                  2)
+                                'ret)
+                    (Get-var 'ret))
+  ; Node not in the list (early return)
+  (check-var-equal? ret 0
+                    (Create-var 'ret 'int)
+                    (Run-method 'get
+                                '((Node
+                                   (Node (None) 1 3 0)
+                                   2 4 0)
+                                  3)
+                                'ret)
+                    (Get-var 'ret)))
