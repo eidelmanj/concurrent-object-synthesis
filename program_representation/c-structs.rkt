1da8efb20b942ab68aaf920b221e5181c4170f2e
Aliya Hameer <adhameer@gmail.com>
2016-06-30 23:46:35 -0400

    Actually working C struct constructors.

--- program_representation/c-structs.rkt
+++ program_representation/c-structs.rkt
@@ -13,45 +13,36 @@
    This solves some of the problems created by using #:auto for default values.
 |#
 
+(require "simulator-structures.rkt")
+
 (require (for-syntax racket/syntax))
 (define-syntax (c-struct stx)
   (syntax-case stx ()
     [(_ id (field-id ...) struct-option ...)
-     (let ([make-id (format-id stx "make-~a" #'id)]
-           [c-id (format-id stx "my-make-~a" #'id)])
-       (with-syntax ([make-id make-id]
-                     [c-id c-id])
+     (let ([alt-id (format-id stx "alt-~a" #'id)]
+           [make-id (format-id stx "make-~a" #'id)])
+       (with-syntax ([alt-id alt-id]
+                     [make-id make-id])
          #'(begin
-             ; Define the struct in a separate module.
-             (module id racket
-               (require (only-in "simulator-structures.rkt"
-                                 C-Instruction))
-               (provide (struct-out make-id))
-               (struct id C-Instruction
-                 (field-id ...)
-                 struct-option ...
-                 #:extra-name make-id))
-
-             ; Import the defined struct under its extra name.
-             (require 'id)
-
-             ; Define a constructor.
-             (define c-id
+             (struct id C-Instruction
+               (field-id ...)
+               struct-option ...
+               #:name alt-id
+               #:constructor-name alt-id)
+             
+             (define make-id
                (case-lambda
-                 [(field-id ...) (make-id null null field-id ...)]
+                 [(field-id ...) (alt-id null null field-id ...)]
                  [(thread-id instr-id field-id ...)
-                  (make-id thread-id instr-id field-id ...)]))
+                  (alt-id thread-id instr-id field-id ...)]))
 
-             ; Bind the id to both a transformer and a constructor.
              (define-match-expander id
-               ; match expander
                (λ (pat)
                  (syntax-case pat ()
-                   [(_ more (... ...)) #'(make-id more (... ...))]))
-               ; constructor
-               (λ (stx)
-                 (syntax-case stx ()
-                   [(_ arg (... ...))
-                    (syntax/loc stx (c-id arg (... ...)))]))))))]))
+                   [(_ more (... ...)) #'(alt-id more (... ...))]))
 
-#;(c-struct Run-method-4 (method args ret))
+               (syntax-id-rules ()
+                 [(_ args (... ...)) (make-id args (... ...))]
+                 [id alt-id])))))]))
+
+#;(c-struct Run-method (method args ret))
