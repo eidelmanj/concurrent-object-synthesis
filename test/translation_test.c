cf943a0566f0185779c79b4f740d09f07cd32439
eidelmanj <eidelmanj@cs.toronto.edu>
2016-09-12 14:03:48 -0400

    Added several missing features to parser

--- test/translation_test.c
+++ test/translation_test.c
@@ -1,33 +1,45 @@
-struct bob {
-    int x;
-    char y;
+
+struct Node {
+  int key;
+  int val;
+  struct Node next;
 
 };
 
-int access()
-{
-    struct bob a; a.x = 1;
-}
 
-int compare(int x, char y, int z) {
-    if(x > z) {
-        char y = 1;
-    } else {
-        char y = 0; 
-    }
-}
-void pointless_loop() {
-    int x = 0; 
-    int lock; 
-    pthhread_mutex_lock(&lock);
-    while(x <= 10) {
-        x = x + 1;
-    }
-}
-    
-void pointless_bool() {
-    int x = 0;
-    int y = 0;
-    if(x && y) {  
-    }
+
+int push(struct Node init, int key, int val) {
+  struct Node *cur;
+  struct Node *prev;
+  struct Node *new;
+
+  cur = init;
+  prev = init;
+  new = (int *)malloc(sizeof(Node));
+
+  if (cur == -1) {
+    new->key = key;
+    new->val = val;
+    return val;
+  }
+  
+  while (cur != -1 && 5==6) {
+    prev = cur;
+    cur = cur->next;
+  }
+
+  if (cur == -1) {
+    new->key = key;
+    new->val = val;
+    prev->next = new;
+    return val;
+  }
+
+  cur->val = val;
+  return val;
+
+
+  
+
+
 }
